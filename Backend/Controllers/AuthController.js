2298e5ecead2c75df4423ace0b36ebe90d1874a6
khushnuma khan <yasminkh053@gmail.com>
2026-09-25 23:13:03 +0530

    Fix signup authentication cookie

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -12,10 +12,10 @@ module.exports.Signup = async (req, res, next) => {
     const user = await User.create({ email, password, username, createdAt });
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
-      sameSite: "none"
-    });
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
     res
       .status(201)
       .json({ message: "User signed in successfully", success: true, user });
@@ -25,7 +25,7 @@ module.exports.Signup = async (req, res, next) => {
   }
 };
 
-
+// for login------------
 module.exports.Login = async (req, res) => {
     try {
 
